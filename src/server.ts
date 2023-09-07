4b62f539d23f27b76087eaa37c7087d18ca6e1b1
Moyo <31387340+MoyosoreCodes@users.noreply.github.com>
2023-09-07 18:40:12 +0100

    removed jsonwebtoken and bcryptjs

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { buildSchema } from "graphql";
 import { config } from "dotenv";
-import * as bcrypt from "bcryptjs";
-import * as jwt from "jsonwebtoken";
 
 config();
 const app = express();
@@ -22,21 +20,21 @@ type User {
 
 type Mutation {
   signUp(username: String!, email: String!, password: String!): User
-  login(email: String!, password: String!): String
+  login(email: String!, password: String!): User
+}
+
+type Query {
+  getUser(email: String): User
 }
 `);
 
 const root = {
   signUp: ({ username, email, password }) => {
-    // Hash the password (you should salt and hash securely in production)
-    const hashedPassword = bcrypt.hashSync(password, 10);
-
-    // Create a new user
     const user = {
       id: users.length + 1,
       username,
       email,
-      password: hashedPassword,
+      password,
     };
     users.push(user);
 
@@ -45,34 +43,14 @@ const root = {
   login: ({ email, password }) => {
     const user = users.find((u) => u.email === email);
 
-    if (!user || !bcrypt.compareSync(password, user.password)) {
+    if (!user || password !== user?.password) {
       throw new Error("Invalid email or password");
     }
 
-    // Generate a JWT token
-    const token = jwt.sign({ userId: user.id }, JWT_SECRET);
-
-    return token;
+    return user;
   },
 };
 
-// app.use("/graphql", (req, res, next) => {
-//   const token = req.header("Authorization");
-
-//   if (!token) {
-//     req.userId = null;
-//     return next();
-//   }
-
-//   jwt.verify(token, JWT_SECRET, (err, decoded) => {
-//     if (err) {
-//       req.userId = null;
-//     } else {
-//       req.userId = decoded.userId;
-//     }
-//     next();
-//   });
-// });
 
 app.use(
   "/graphql",
